02e5b4926601a4f94e045632111ddcd25a87add1
Marto-meeshop <genchev.martin@gmail.com>
2022-08-13 20:12:33 +0200

    Reusable Component init

---
+++ src/components/FormExample.vue
@@ -0,0 +1,32 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <input v-model="message" @input="$emit('input', $event.target.value)" placeholder="edit me">
+    <p>Message is: {{ message }}</p>
+    <p>Message is: {{ messageTwo }}</p>
+    <button @click="handleMessage"> CLICK</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormExample',
+  props: {
+    messageTwo: {
+      type: String,
+      required: false
+    }
+  },
+  data () {
+    return {
+      message: ' Reuse me',
+    }
+  },
+  methods: {
+    handleMessage () {
+      const messageForEmit = this.messageTwo + this.message
+      this.$emit('click', messageForEmit)
+    }
+  }
+}
+</script>
